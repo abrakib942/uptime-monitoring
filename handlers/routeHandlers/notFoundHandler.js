f554e0197861dc7a4d6e66507e610494fea69186
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2022-11-07 12:34:02 +0600

    first commit

---
+++ handlers/routeHandlers/notFoundHandler.js
@@ -0,0 +1,10 @@
+// module scaffolding
+const handler = {};
+
+handler.notFoundHandler = (requestProperties, callback) => {
+  callback(404, {
+    message: "your requested url was not found",
+  });
+};
+
+module.exports = handler;
---
+++ index.js
@@ -0,0 +1,45 @@
+// dependencies
+const http = require("http");
+const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environments");
+const data = require("./lib/data");
+
+// app object = module scaffolding
+const app = {};
+
+// data.create(
+//   "test",
+//   "newFile",
+//   { name: "Bangladesh", language: "Bengali" },
+//   function (err) {
+//     console.log(`error was`, err);
+//   }
+// );
+// data.read("test", "newFile", (err, result) => {
+//   console.log(err, result);
+// });
+// data.update(
+//   "test",
+//   "newFile",
+//   { name: "England", language: "English" },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+data.delete("test", "newFile", (err) => {
+  console.log(err);
+});
+
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(environment.port, () => {
+    console.log(`listening to port ${environment.port}`);
+  });
+};
+
+// handle Request Response
+app.handleReqRes = handleReqRes;
+
+// start server
+app.createServer();
